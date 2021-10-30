c0f180a6e8f715542cbc05156431b9012b6fdfb2
riwexoyd <alan.walterk@gmail.com>
2021-10-30 20:54:19 +0300

    Added some components

---
+++ TerrainGenerator.BlazorWebApp/Models/TerrainModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using TerrainGenerator.Models;
+
+namespace TerrainGenerator.BlazorWebApp.Models
+{
+    public class TerrainModel
+    {
+        private readonly ITerrain _terrain;
+
+        public TerrainModel(ITerrain terrain)
+        {
+            _terrain = terrain;
+        }
+
+        public int Columns => _terrain.Columns;
+
+        public int Rows => _terrain.Rows;
+
+
+    }
+}
---
+++ TerrainGenerator.BlazorWebApp/Models/WorkSpaceModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TerrainGenerator.BlazorWebApp.Models
+{
+    public class WorkSpaceModel
+    {
+        public TerrainModel CurrentTerrain { get; set; }
+    }
+}
